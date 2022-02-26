96ff858d3a3571d8e072a2864eb0d20465c7987e
laekenrg <laeken.rosado@gmail.com>
2022-02-26 17:48:04 +0100

    cambios restantes L

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,11 @@
 function mentores(mentor1, mentor2, mentor3, mentor4, mentor5) {
-  return mentor1.toUpperCase();
+  return (
+    mentor1.toUpperCase() +
+    mentor2.toUpperCase() +
+    mentor3.toUpperCase() +
+    mentor4.toUpperCase() +
+    mentor5.toUpperCase()
+  );
 }
 function saludoCompleto(saludo, mentor1, mentor2, mentor3, mentor4, mentor5) {
   return (
